09aa25da0d6d701d86879176a2f051df8f768e0e
MarkZakelj <markzakelj@gmail.com>
2024-06-08 16:59:21 +0100

    update paths

--- utils/const.py
+++ utils/const.py
@@ -1,7 +1,8 @@
 import os
 
 script_dir = os.path.dirname(__file__)
-data_dir = os.path.join(script_dir, "data")
+root_dir = os.path.abspath(os.path.join(script_dir, ".."))
+data_dir = os.path.join(root_dir, "data")
 img_dir = os.path.join(data_dir, "images")
 faces_dir = os.path.join(data_dir, "faces")
-checkpoints_dir = os.path.join(script_dir, "checkpoints")
+checkpoints_dir = os.path.join(root_dir, "checkpoints")
